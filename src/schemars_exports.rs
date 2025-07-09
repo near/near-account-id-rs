25cd4563a57fd49a4aa15231a075aa339a08bd68
polyprogrammist <vad.e.volodin@gmail.com>
2025-07-09 21:29:18 +0800

    remove schemars-stable and schemars-alpha from dependencies

--- src/schemars_exports.rs
+++ src/schemars_exports.rs
@@ -1,5 +1,5 @@
 #[cfg(all(feature = "schemars-alpha", not(feature = "schemars-stable"), not(feature = "schemars-v1"), not(feature = "schemars-v0_8")))]
-pub use schemars_alpha as schemars;
+pub use schemars_v1 as schemars;
 
 #[cfg(all(feature = "schemars-v1", not(feature = "schemars-alpha"), not(feature = "schemars-stable"), not(feature = "schemars-v0_8")))]
 pub use schemars_v1 as schemars;
@@ -8,4 +8,4 @@ pub use schemars_v1 as schemars;
 pub use schemars_v0_8 as schemars;
 
 #[cfg(feature = "schemars-stable")]
-pub use schemars_stable as schemars;
+pub use schemars_v0_8 as schemars;
