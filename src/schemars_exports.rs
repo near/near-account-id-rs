0200fe0933cdaae89a61ea3cba9808283379047f
polyprogrammist <vad.e.volodin@gmail.com>
2025-07-09 20:14:51 +0800

    Add schemars-v1 and schemars-v0_8 features, reverts schemars-stable to 0.8.22

--- src/schemars_exports.rs
+++ src/schemars_exports.rs
@@ -1,5 +1,11 @@
-#[cfg(all(feature = "schemars-alpha", not(feature = "schemars-stable")))]
+#[cfg(all(feature = "schemars-alpha", not(feature = "schemars-stable"), not(feature = "schemars-v1"), not(feature = "schemars-v0_8")))]
 pub use schemars_alpha as schemars;
+
+#[cfg(all(feature = "schemars-v1", not(feature = "schemars-alpha"), not(feature = "schemars-stable"), not(feature = "schemars-v0_8")))]
+pub use schemars_v1 as schemars;
+
+#[cfg(all(feature = "schemars-v0_8", not(feature = "schemars-v1"), not(feature = "schemars-alpha"), not(feature = "schemars-stable")))]
+pub use schemars_v0_8 as schemars;
 
 #[cfg(feature = "schemars-stable")]
 pub use schemars_stable as schemars;
