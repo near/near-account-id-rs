e1c7e410f7a43bc8bb47f894175b43d888e702fa
polyprogrammist <vad.e.volodin@gmail.com>
2025-07-10 22:08:05 +0800

    updates as discussed

--- src/schemars_exports.rs
+++ src/schemars_exports.rs
@@ -1,11 +1,8 @@
-#[cfg(all(feature = "schemars-alpha", not(feature = "schemars-stable"), not(feature = "schemars-v1"), not(feature = "schemars-v0_8")))]
+#[cfg(feature = "schemars-v1")]
 pub use schemars_v1 as schemars;
 
-#[cfg(all(feature = "schemars-v1", not(feature = "schemars-alpha"), not(feature = "schemars-stable"), not(feature = "schemars-v0_8")))]
-pub use schemars_v1 as schemars;
-
-#[cfg(all(feature = "schemars-v0_8", not(feature = "schemars-v1"), not(feature = "schemars-alpha"), not(feature = "schemars-stable")))]
+#[cfg(feature = "schemars-v0_8")]
 pub use schemars_v0_8 as schemars;
 
-#[cfg(feature = "schemars-stable")]
-pub use schemars_v0_8 as schemars;
+#[cfg(all(feature = "schemars-v1", feature = "schemars-v0_8"))]
+compile_error!("features schemars-v1 and schemars-v0_8 are not allowed to be enabled at the same time. Only 1 of them is allowed");
